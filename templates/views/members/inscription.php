29b4cd73286d8c04673a14af4e0aaae347b9de5e
fukushimiste <vanlong.creeperlol@gmail.com>
2015-08-18 14:12:24 +0200

    Membres - Page Inscription

---
+++ templates/views/members/inscription.php
@@ -0,0 +1,36 @@
+<div class="inscription">
+	<fieldset>
+	<legend>Inscription</legend>
+		<form class="form-horizontal">
+			<div class="form-group">
+				<label class="col-sm-2 control-label" for="cPseudo">Pseudonyme (*)</label>
+				<div class="col-sm-10">
+					<input class="form-control" type="text" name="cPseudo" placeholder="Votre pseudonyme" required/>
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-sm-2 control-label" for="cPseudo">Email (*)</label>
+				<div class="col-sm-10">
+					<input class="form-control" type="text" name="cPseudo" placeholder="Votre email" required/>
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-sm-2 control-label" for="cPseudo">Mot de passe (*)</label>
+				<div class="col-sm-10">
+					<input class="form-control" type="text" name="cPseudo" placeholder="Votre mot de passe" required/>
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-sm-2 control-label" for="cCmdp">Confirmation (*)</label>
+				<div class="col-sm-10">
+					<input class="form-control" type="text" name="cCmdp" placeholder="Confirmer votre mot de passe" required/>
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="col-sm-offset-2 col-sm-10">
+					<input class="form-control" class="btn" type="submit" value="S'inscrire" required/>
+				</div>
+			</div>
+		</form>
+	</fieldset>
+</div>
