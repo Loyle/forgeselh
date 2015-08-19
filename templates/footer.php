f276dff78f28d5141a4a0e7582b41a38784a1e5d
fukushimiste <vanlong.creeperlol@gmail.com>
2015-08-19 20:11:31 +0200

    1 commit

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,8 @@
 		</div>
 			<footer class="footer">
-			Copyright Forgeselh 2015
+			  <div class="container">
+				<p class="text-muted">Copyright Forgeselh 2015</p>
+			  </div>
 			</footer>
 		<script src="<?php echo base_url(); ?>templates/js/jquery-2.1.1.min.js"></script>
 		<script src="<?php echo base_url(); ?>templates/js/bootstrap.min.js"></script>
